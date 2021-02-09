8ce48efbcfc0d99fbdab7796d63c9ef5f0f59be7
Egor <hotcoldwave@gmail.com>
2021-02-09 04:47:25 +0300

    First

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php wp_head();?>
+    
+</head>
+<body>
+
+<header>
+    <div class="container">
+    <?php
+    wp_nav_menu(
+        array(
+            'theme_location' => 'top-menu',
+            'menu_class' => 'top-bar',
+            // 'menu' => 'Tob Bar',
+        )
+    );
+    ?>
+    </div>  
+</header>
+    
