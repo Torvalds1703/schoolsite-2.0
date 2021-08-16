652684494d50679de7bf94a3f0c88743df0f46de
hotcoldwave <hotcoldwave@gmail.com>
2021-08-16 19:49:10 +0300

    fixes

--- footer.php
+++ footer.php
@@ -1,17 +1,15 @@
 </div>
-<footer class="footer mt-4">
-
-<?php wp_nav_menu([
-            "theme_location" => "footer-menu",
-            "depth" => 2,
-            "container" => "nav",
-            "container_class" =>
-            "collapse navbar-collapse",
-            "container_id" => "bs-example-navbar-collapse-1",
-            "menu_class" => "navbar-footer",
-          ]); ?>
+  <footer class="footer">
+    <?php wp_nav_menu([
+                "theme_location" => "footer-menu",
+                "depth" => 1,
+                "container" => "nav",
+                "container_class" =>
+                "collapse navbar-collapse",
+                "container_id" => "bs-example-navbar-collapse-1",
+                "menu_class" => "navbar-footer",
+              ]); ?>
     <?php wp_footer(); ?>
-
   </footer>
 </body>
 </html>
