c3d1afc7e6b64352f448a59bf7b854196cea5f49
hotcoldwave <hotcoldwave@gmail.com>
2021-08-16 20:37:35 +0300

    aside banners links

--- sidebar.php
+++ sidebar.php
@@ -10,27 +10,39 @@
                     <?php dynamic_sidebar('page-sidebar'); ?>
             <?php endif; ?>
 
-            <a href="#" class="aside__banner">
+            <a href="<?php echo site_url(
+                "/nezavisimaya-oczenka-kachestva-obrazovaniya/"
+                ); ?>" class="aside__banner">
                   <img src="<?php echo get_theme_file_uri('/images/banners/banner-edu.png'); ?>" alt="">
             </a>
 
-            <a href="#" class="aside__banner">
+            <a href="<?php echo site_url(
+                "/priem-v-pervyj-klass-v-2021-godu/"
+                ); ?>" class="aside__banner">
                   <img src="<?php echo get_theme_file_uri('/images/banners/banner-first.png'); ?>" alt="">
             </a>
 
-            <a href="#" class="aside__banner">
+            <a href="<?php echo site_url(
+                "/usloviya/pitanie/"
+                ); ?>" class="aside__banner">
                   <img src="<?php echo get_theme_file_uri('/images/banners/banner-dining.png'); ?>" alt="">
             </a>
 
-            <a href="#" class="aside__banner">
+            <a href="<?php echo site_url(
+                "/ezhednevnoe-menyu/"
+                ); ?>" class="aside__banner">
                   <img src="<?php echo get_theme_file_uri('/images/banners/banner-menu.png'); ?>" alt="">
             </a>
 
-            <a href="#" class="aside__banner">
+            <a href="<?php echo site_url(
+                "/deyatelnost/shkolnyj-sportivnyj-klub-druzhba/"
+                ); ?>" class="aside__banner">
                   <img src="<?php echo get_theme_file_uri('/images/banners/banner-drujba.png'); ?>" alt="">
             </a>
 
-            <a href="#" class="aside__banner">
+            <a href="<?php echo site_url(
+                "/deyatelnost/doshkolnoe-otdelenie/"
+                ); ?>" class="aside__banner">
                   <img src="<?php echo get_theme_file_uri('/images/banners/banner-odod.png'); ?>" alt="">
             </a>
       </aside>
