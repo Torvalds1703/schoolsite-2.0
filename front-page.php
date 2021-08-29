f681bb3c83ce7a9303faafe42de91d308545fc92
hotcoldwave <hotcoldwave@gmail.com>
2021-08-30 01:02:06 +0300

    director styles

--- front-page.php
+++ front-page.php
@@ -3,8 +3,21 @@
 <div class="container-fluid">
   <div class="row align-content-center">
     <div class="col-12 col-lg-9">
-      <div class="row flex-column flex-lg-row">
-        <div class="col-12 order-2 col-xl-6">
+      <div class="">
+      <!-- <div class="col-12 order-1 col-xl-6">
+            <img
+              src="<?php echo get_theme_file_uri('/images/director.jpg'); ?>"
+              alt="Фотография директора школы"
+              class="img-fluid rounded main__director-photo"
+            />
+      </div> -->
+
+        <div class="col-12 order-2 ">
+          <img
+              src="<?php echo get_theme_file_uri('/images/director.jpg'); ?>"
+              alt="Фотография директора школы"
+              class="img-fluid rounded main__director-photo"
+            />
           <blockquote class="citation citation-success">
             <div class="citation-body">
             <p>Дорогие гости нашего сайта, я рада приветствовать вас! Наша школа является единственным образовательным учреждением в России, предоставляющим услуги по  углубленному изучению языка хинди, и в этом ее уникальность. В условиях всестороннего развития российско-индийских международных отношений знание языка хинди все больше будет востребовано как в экономической, так и в культурной сфере и предоставит своим владельцам перспективы выгодного трудоустройства.</p> 
@@ -16,17 +29,11 @@
             </div>
           </blockquote>
         </div>
-        <div class="col-12 order-1 col-xl-6">
-          <img
-            src="<?php echo get_theme_file_uri('/images/director.jpg'); ?>"
-            alt="Фотография директора школы"
-            class="img-fluid rounded main__director-photo"
-          />
-        </div>
+    
         </div>
         <div class="row mt-3">
           <div class="col-12 pl-4">
-          <?php get_template_part('includes/section', 'frontcontent'); ?>
+            <?php get_template_part('includes/section', 'frontcontent'); ?>
           </div>
       </div>
     </div>
