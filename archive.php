3c93559fc6eec71c06e6e2cc8858e45e58a4a507
hotcoldwave <hotcoldwave@gmail.com>
2021-08-30 00:03:34 +0300

    archive page styles

--- archive.php
+++ archive.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="container-fluid mt-3">
+<div class="container-fluid mt-3 news-archive">
   <div class="row align-content-center">
     <div class="col-12 col-lg-9">
       <?php get_template_part("includes/section", "archive"); ?>
