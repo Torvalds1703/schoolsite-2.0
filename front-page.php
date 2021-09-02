9e333390fe315077d6aabc60e981bf9617fabadf
hotcoldwave <hotcoldwave@gmail.com>
2021-09-02 16:00:28 +0300

    gu-widget

--- front-page.php
+++ front-page.php
@@ -1,6 +1,43 @@
 <?php get_header(); ?>
 
 <div class="container-fluid">
+
+<div class="row"> 
+  <div class="col">
+  <div id='js-show-iframe-wrapper'>
+    <div class='pos-banner-fluid bf-23'>
+      <div class='bf-23__decor'>
+        <div class='bf-23__logo-wrap'>
+          <img
+            class='bf-23__logo'
+            src='https://pos.gosuslugi.ru/bin/banner-fluid/gosuslugi-logo-blue.svg'
+            alt='Госуслуги'
+          />
+          <div class='bf-23__slogan'>Решаем вместе</div>
+        </div>
+      </div>
+      <div class='bf-23__content'>
+        <div class='bf-23__text'>
+          Есть предложения по организации учебного процесса или знаете, как сделать школу лучше?
+        </div>
+
+        <div class='bf-23__bottom-wrap'>
+          <div class='bf-23__btn-wrap'>
+            <!-- pos-banner-btn_2 не удалять; другие классы не добавлять -->
+            <button
+              class='pos-banner-btn_2'
+              type='button'
+            >Написать о проблеме
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  <script>Widget("https://pos.gosuslugi.ru/form", 262465)</script>
+
   <div class="row align-content-center">
     <div class="col-12 col-lg-9">
       <div class="">
