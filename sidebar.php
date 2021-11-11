140c3b93fedd90e912ed557fbaa00bc44ce756bc
hotcoldwave <hotcoldwave@gmail.com>
2021-11-11 12:07:54 +0300

    banner

--- sidebar.php
+++ sidebar.php
@@ -6,11 +6,11 @@
                   <div><span></span>Информация для родителей (законных представителей) о режиме функционирования ОУ в условиях распространения COVID-19</div>
             </a>
             
-            <!-- <a href="<?php echo site_url(
+            <a href="<?php echo site_url(
                 "/wp-content/uploads/2021/10/perepisnye-uchastki.pdf"
                 ); ?>" class="aside__banner--covid">
                   <div style="margin-top: 20px">Переписные участки в Калининском районе СПб</div>
-            </a> -->
+            </a>
 
             <?php if (is_active_sidebar('page-sidebar')): ?>
                     <?php dynamic_sidebar('page-sidebar'); ?>
